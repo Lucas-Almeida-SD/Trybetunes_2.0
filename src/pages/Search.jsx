8b8d1f8272a0f1967930142f7de56da7394b4a0d
Lucas <lucas.almdiniz@gmail.com>
2024-06-27 21:25:13 -0400

    feat: Requisito 5 - formulário para pesquisar artistas

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,9 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Search() {
+  const [searchInput, setSearchInput] = useState('');
+  const [isEnableBtn, setIsEnableBtn] = useState(false);
+
+  const handleChange = ({ target }) => {
+    setSearchInput(target.value);
+    setIsEnableBtn(target.value.length >= 2);
+  };
+
   return (
     <div data-testid="page-search">
       <h1>Search</h1>
+      <form action="">
+        <input
+          data-testid="search-artist-input"
+          type="text"
+          value={ searchInput }
+          onChange={ handleChange }
+        />
+        <button
+          data-testid="search-artist-button"
+          type="submit"
+          disabled={ !isEnableBtn }
+        >
+          Pesquisar
+        </button>
+      </form>
     </div>
   );
 }
