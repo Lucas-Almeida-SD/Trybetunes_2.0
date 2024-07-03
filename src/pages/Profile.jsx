a617f3e71ba2fdf59d7de30cf42bc66acc10ce85
Lucas <lucas.almdiniz@gmail.com>
2024-07-02 20:30:36 -0400

    feat: Requisito 13 - Crie a exibição de perfil

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,9 +1,50 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+
+import Loading from '../components/Loading';
+
+import { getUser } from '../services/userAPI';
+import routes from '../utils/routes';
 
 function Profile() {
+  const [user, setUser] = useState(undefined);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    const getUserApi = async () => {
+      setIsLoading(true);
+      const response = await getUser();
+      setUser(response);
+      setIsLoading(false);
+    };
+
+    getUserApi();
+  }, []);
+
   return (
     <div data-testid="page-profile">
-      <h1>Profile</h1>
+      {isLoading && <Loading />}
+      {user && (
+        <div>
+          <div>
+            <img data-testid="profile-image" src={ user.image } alt={ user.name } />
+            <Link to={ routes.profileEdit }>Editar perfil</Link>
+          </div>
+          <div>
+            <span>Nome</span>
+            <span>{user.name}</span>
+          </div>
+          <div>
+            <span>Email</span>
+            <span>{user.email}</span>
+          </div>
+          <div>
+            <span>Descrição</span>
+            <p>{user.description}</p>
+          </div>
+        </div>
+      )}
+
     </div>
   );
 }
