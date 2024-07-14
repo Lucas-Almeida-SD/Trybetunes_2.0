7379b0f5f1eb826be988df889c6183f3a5108fe9
Lucas <lucas.almdiniz@gmail.com>
2024-07-14 12:08:23 -0400

    refactor: Adicionadas infos da logo do APP e de usuario; Impotada estilização do Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,6 +4,10 @@ import { NavLink } from 'react-router-dom';
 import { getUser } from '../services/userAPI';
 import Loading from './Loading';
 import routes from '../utils/routes';
+import songIcon from '../assets/images/song_icon.png';
+import userIcon from '../assets/images/user.png';
+
+import '../styles/Header.scss';
 
 function Header() {
   const [user, setUser] = useState(null);
@@ -20,16 +24,28 @@ function Header() {
     getUserName();
   }, []);
 
-  if (isLoading) {
-    return (
-      <Loading />
-    );
-  }
-
   return (
-    <header data-testid="header-component">
-      {user && <span data-testid="header-user-name">{user.name}</span>}
-      <nav>
+    <header data-testid="header-component" id="page-header">
+      <div id="page-info">
+        <div id="page-logo-container">
+          <div id="page-icon-container">
+            <img src={ songIcon } alt="Ícone da Aplicação" />
+          </div>
+          <h1 id="page-title">Trybetunes</h1>
+        </div>
+        <div id="user-conatiner">
+          {user && !isLoading ? (
+            <>
+              <div id="user-image-container">
+                <img src={ user.image || userIcon } alt="Imagem do usuário" />
+              </div>
+              <span data-testid="header-user-name" id="user-name">{user.name}</span>
+            </>
+          ) : <Loading />}
+        </div>
+      </div>
+
+      <nav id="page-nav">
         <NavLink
           data-testid="link-to-search"
           to={ routes.search }
