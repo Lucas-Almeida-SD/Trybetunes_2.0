048c554f123b4d114d498fa90a07ba5508ed7d28
Lucas <lucas.almdiniz@gmail.com>
2024-07-14 18:01:43 -0400

    refactor: Mundança no check de favoritar, classname e outros
    Checkbox de favoritar musica removido e adicionado icones de favoritar musica no seu lugar; adicionado capa do album; Adicionado classname nos elementos; Estilização importada

--- src/components/MusicCard.jsx
+++ src/components/MusicCard.jsx
@@ -1,13 +1,17 @@
 import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
-
 import { useLocation } from 'react-router-dom';
+
 import { addSong, removeSong, getFavoriteSongs } from '../services/favoriteSongsAPI';
 import routes from '../utils/routes';
+import heartIcon from '../assets/images/heart.png';
+import filledHeartIcon from '../assets/images/filled_heart.png';
+
+import '../styles/MusicCard.scss';
 
 function MusicCard({ musicData, setIsLoading, favoriteMusicList, setFavoriteMusicList }) {
   const [isFavorite, setIsFavorite] = useState(false);
-  const { trackName, previewUrl, trackId } = musicData;
+  const { trackName, previewUrl, trackId, artworkUrl100 } = musicData;
   const location = useLocation();
 
   const handlePlayAudio = ({ target }) => {
@@ -21,15 +25,17 @@ function MusicCard({ musicData, setIsLoading, favoriteMusicList, setFavoriteMusi
     target.classList.add('selected');
   };
 
-  const handleChangeFavorite = async ({ target }) => {
+  const handleClickFavorite = async () => {
+    const updateIsFavorite = !isFavorite;
+
     setIsLoading(true);
-    if (target.checked) {
+    if (updateIsFavorite) {
       await addSong(musicData);
     } else {
       await removeSong(musicData);
     }
 
-    setIsFavorite(!target.checked);
+    setIsFavorite(updateIsFavorite);
 
     if (location.pathname === routes.favorites) {
       const favoriteMusicListUpdated = await getFavoriteSongs();
@@ -52,26 +58,27 @@ function MusicCard({ musicData, setIsLoading, favoriteMusicList, setFavoriteMusi
   }, []);
 
   return (
-    <div>
-      <span>{trackName}</span>
-      <audio
-        onPlay={ handlePlayAudio }
-        data-testid="audio-component"
-        src={ previewUrl }
-        controls
-      >
-        <track kind="captions" />
-      </audio>
-      <label htmlFor={ trackId }>
-        <input
-          data-testid={ `checkbox-music-${trackId}` }
-          type="checkbox"
-          id={ trackId }
-          onChange={ handleChangeFavorite }
-          checked={ isFavorite }
-        />
-        <span>Favorita</span>
-      </label>
+    <div className="music-card-container">
+      <img src={ artworkUrl100 } alt="Imagem do álbum" className="album-music-image" />
+      <div className="audio-info-container">
+        <audio
+          onPlay={ handlePlayAudio }
+          data-testid="audio-component"
+          src={ previewUrl }
+          controls
+          className="music-audio"
+        >
+          <track kind="captions" />
+        </audio>
+        <span className="music-name">{trackName}</span>
+      </div>
+      <img
+        src={ isFavorite ? filledHeartIcon : heartIcon }
+        alt="Icone de música favoritada"
+        onClick={ handleClickFavorite }
+        role="presentation"
+        className="favorite-music-icon"
+      />
     </div>
   );
 }
@@ -83,6 +90,7 @@ MusicCard.propTypes = {
     trackName: PropTypes.string.isRequired,
     previewUrl: PropTypes.string.isRequired,
     trackId: PropTypes.number.isRequired,
+    artworkUrl100: PropTypes.string.isRequired,
   }).isRequired,
   setIsLoading: PropTypes.func.isRequired,
   favoriteMusicList: PropTypes.arrayOf(PropTypes.shape({
