55b6d9f424949717a595274bcba33b6127f446c1
Lucas <lucas.almdiniz@gmail.com>
2024-07-28 17:26:44 -0400

    feat: Atualiza a imagem de usuário
    Também foram adicionados IDs, Classnames e foi importado arquivo de estilização

--- src/pages/ProfileEdit.jsx
+++ src/pages/ProfileEdit.jsx
@@ -6,6 +6,9 @@ import Loading from '../components/Loading';
 import { getUser, updateUser } from '../services/userAPI';
 import validateUser from '../utils/validateUser';
 import routes from '../utils/routes';
+import userIcon from '../assets/images/user.png';
+
+import '../styles/ProfileEdit.scss';
 
 function ProfileEdit() {
   const [user, setUser] = useState(undefined);
@@ -14,24 +17,12 @@ function ProfileEdit() {
   const [isValidFields, setIsValidFields] = useState(false);
   const navigate = useNavigate();
 
-  useEffect(() => {
-    const getUserApi = async () => {
-      setIsLoading(true);
-      const response = await getUser();
-      setUser(response);
-      setIsValidFields(validateUser(response));
-      setIsLoading(false);
-    };
-
-    getUserApi();
-  }, []);
-
   const previewImage = (element) => {
     const file = element.files[0];
     const reader = new FileReader();
 
     reader.addEventListener('load', () => {
-      setImagePath(reader.result);
+      setUser({ ...user, image: reader.result });
     });
 
     if (file) {
@@ -39,15 +30,40 @@ function ProfileEdit() {
     }
   };
 
+  useEffect(() => {
+    const getUserApi = async () => {
+      setIsLoading(true);
+
+      const response = await getUser();
+      setUser({ ...response, image: response.image || userIcon });
+
+      setIsValidFields(validateUser(response));
+
+      setIsLoading(false);
+    };
+
+    getUserApi();
+  }, []);
+
+  useEffect(() => {
+    const refresh = () => window.location.reload();
+
+    return refresh;
+  }, []);
+
+  useEffect(() => {
+    if (user) setIsValidFields(validateUser(user));
+  }, [user]);
+
   const handleChangeUser = ({ target }) => {
     const { name, value } = target;
-    setUser((previous) => ({ ...previous, [name]: value }));
 
     if (name === 'image') {
+      setImagePath(target.value);
       previewImage(target);
+    } else {
+      setUser((previous) => ({ ...previous, [name]: value }));
     }
-
-    setIsValidFields(validateUser({ ...user, [name]: value }));
   };
 
   const handleClick = async () => {
@@ -57,53 +73,72 @@ function ProfileEdit() {
     navigate(routes.profile);
   };
 
+  const editImageAfterClickOnImage = () => {
+    const editImageInput = document.querySelector('#edit-image-input');
+    console.log();
+    editImageInput.click();
+  };
+
   return (
-    <div data-testid="page-profile-edit">
+    <div data-testid="page-profile-edit" className="page" id="profile-edit-page">
       {isLoading && <Loading />}
       {user && (
-        <div>
-          <form action="">
-            <div>
-              <img src={ imagePath } alt={ user.name } />
+        <div id="profile-edit-page">
+          <form action="" id="profile-edit-form">
+            <div className="image-container info-container">
+              <div className="image-background">
+                <img
+                  src={ user.image }
+                  alt={ user.name }
+                  className="image-container"
+                  onClick={ editImageAfterClickOnImage }
+                  role="presentation"
+                />
+              </div>
               <input
                 data-testid="edit-input-image"
                 type="file"
                 accept="image/*"
                 name="image"
-                value={ user.image }
+                id="edit-image-input"
+                value={ imagePath }
                 onChange={ handleChangeUser }
+                style={ { display: 'none' } }
               />
             </div>
-            <div>
-              <label htmlFor="user-name">
-                <span>Nome</span>
+            <div id="info-section">
+              <label htmlFor="user-name" className="info-container">
+                <span className="info-label">Nome</span>
                 <input
                   data-testid="edit-input-name"
                   type="text"
                   id="user-name"
+                  className="info"
                   name="name"
                   value={ user.name }
                   onChange={ handleChangeUser }
 
                 />
               </label>
-              <label htmlFor="user-email">
-                <span>Email</span>
+              <label htmlFor="user-email" className="info-container">
+                <span className="info-label">Email</span>
                 <input
                   data-testid="edit-input-email"
                   type="text"
                   id="user-email"
+                  className="info"
                   name="email"
                   value={ user.email }
                   onChange={ handleChangeUser }
                 />
               </label>
-              <label htmlFor="user-description">
-                <span>Descrição</span>
-                <input
+              <label htmlFor="user-description" className="info-container">
+                <span className="info-label">Descrição</span>
+                <textarea
                   data-testid="edit-input-description"
                   type="text"
                   id="user-description"
+                  className="info"
                   name="description"
                   value={ user.description }
                   onChange={ handleChangeUser }
@@ -113,6 +148,7 @@ function ProfileEdit() {
             <button
               data-testid="edit-button-save"
               type="button"
+              className="profile-edit-btn"
               onClick={ handleClick }
               disabled={ !isValidFields }
             >
