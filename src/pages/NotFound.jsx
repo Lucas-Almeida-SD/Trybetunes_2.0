c437a8dcd78dbc6b7da41ccc8064c777e5e227ca
Lucas <lucas.almdiniz@gmail.com>
2024-08-03 19:11:56 -0400

    refactor: Adicionado elemento de imagem para exibir imagem de not found

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 
+import notFoundImg from '../assets/images/not_found.png';
+
+import '../styles/NotFound.scss';
+
 function NotFound() {
   return (
-    <div data-testid="page-not-found">
-      <h1>NotFound</h1>
+    <div data-testid="page-not-found" className="page" id="not-found-page">
+      <img src={ notFoundImg } alt="Página não encontrada" />
     </div>
   );
 }
